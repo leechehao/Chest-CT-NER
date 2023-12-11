e678d757d37cc404a17c99b3a647073cf8239902
bryant <leehao90301@gmail.com>
2023-12-11 10:52:17 +0800

    [Fix] 驗證階段確定 new_data 位置裡的內容是否為檔案
    [Describe]

--- 2_data_validation/data_validation.py
+++ 2_data_validation/data_validation.py
@@ -13,9 +13,12 @@ def main():
     with open("2_data_validation/schema.json") as file:
         schema =json.load(file)
 
-    for file_path in os.listdir(NEW_DATA_DIR):
-        print(f"驗證 {file_path} 資料中...")
-        with open(os.path.join(NEW_DATA_DIR, file_path)) as file:
+    for file_name in os.listdir(NEW_DATA_DIR):
+        file_path = os.path.join(NEW_DATA_DIR, file_name)
+        if not os.path.isfile(file_path):
+            continue
+        print(f"驗證 {file_name} 資料中...")
+        with open(file_path) as file:
             json_data = json.load(file)
             for i, instance in enumerate(json_data):
                 try:
@@ -24,7 +27,7 @@ def main():
                     print(f"第{i}筆 JSON data 無效 >_<")
                     print("Error message:", e.message)
                     sys.exit(1)
-            print(f"恭喜 {file_path} 資料有效 ^_^")
+            print(f"恭喜 {file_name} 資料有效 ^_^")
 
 
 if __name__ == '__main__':
